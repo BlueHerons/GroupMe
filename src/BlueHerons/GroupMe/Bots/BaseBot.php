94396005614cb717f6d73eb19574390c83380986
johnluetke <john@johnluetke.net>
2015-07-18 20:46:46 +0000

    Adding ability to search and tag users in messages

--- src/BlueHerons/GroupMe/Bots/BaseBot.php
+++ src/BlueHerons/GroupMe/Bots/BaseBot.php
@@ -18,6 +18,7 @@ abstract class BaseBot {
 
     private $bot_id;
     private $payload;
+    private $group;
 
     public function __construct($token, $bot_id) {
         $this->gm = new GroupMePHP\groupme($token);
@@ -40,7 +41,7 @@ abstract class BaseBot {
                 $this->config = $config->{$bot_id};
             }
             else {
-                $this->logger->debug("No configuration for this bot");
+                $this->logger->debug("No configuration for " . $bot_id);
                 $this->config = (object) array(
                     "admin" => array(),
                     "blacklist" => array()
@@ -174,7 +175,12 @@ abstract class BaseBot {
      * @param $msg the message to send
      */
     protected function sendMessage($msg) {
-        $this->sendGroupMessage($msg, $this->getGroupID());
+        try {
+            $this->sendGroupMessage($msg, $this->getGroupID());
+        }
+        catch (Exception $e) {
+            $this->logger->fatal("EXCEPTION WHILE SENDING MESSAGE", $e);
+        }
     }
 
     /**
@@ -185,17 +191,146 @@ abstract class BaseBot {
      * @param $group_id the group id
      */
     protected function sendGroupMessage($msg, $group_id) {
-        $this->gm->messages->create($group_id, array(
-            uniqid(),
-            $msg
+        //$msg = print_r($this->getMentions($msg), true);
+        $this->logger->debug(sprintf("Sending '%s' to group %s", $msg, $group_id));
+
+        sleep(1);
+        $result = $this->gm->messages->create($group_id, array(
+            md5(time() . uniqid()),
+            $msg,
+            $this->getMentions($msg)
         ));
+        $this->logger->debug(print_r($result, true));
     }
 
     /**
      * Gets the GroupMe group ID that triggered the message
      */
     protected function getGroupID() {
-        return $this->payload['group_id'];
+        $group_id = "";
+        if (isset($this->payload) && isset($this->payload['group_id'])) {
+            $group_id = $this->payload['group_id'];
+        }
+        else {
+            //throw new \Exception("Group ID unavailable");
+            $group_id = 15033208;
+        }
+        $this->logger->debug("Group id: " . $group_id);
+        return $group_id;
+    }
+
+    /**
+     * Gets information about the group that the message was sent from
+     */
+    protected function getGroupInfo() {
+        if ($this->group == null) {
+            $this->group = json_decode($this->gm->groups->show($this->getGroupID()))->response;
+            $m = $this->group->members;
+            usort($m, function($a, $b) {
+                return strcmp($a->nickname, $b->nickname);
+            });
+            $this->group->members = $m;
+        }
+
+        return $this->group;
+    }
+
+    /**
+     * Gets a list of members in the group
+     */
+    protected function getGroupMembers() {
+        return $this->getGroupInfo()->members;
+    }
+
+    /**
+     * Searches the group members for a member having the given name, and returns information
+     * about them
+     */
+    protected function getMemberByName($name) {
+        foreach ($this->getGroupMembers() as $member) {
+            if ($member->nickname === $name) {
+                return $member;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Searches the group members for a member having the given name, and returns information
+     * about them
+     */
+    protected function getMemberByID($id) {
+        $id .= ""; // force string
+        foreach ($this->getGroupMembers() as $member) {
+            if ($member->user_id === $id) {
+                return $member;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Searches the group members for a member whose nickname matches the $partial_name parameter.
+     * The first match is returned, or false if no matches
+     *
+     * @param $partial_name name to use for a search.
+     */
+    protected function searchMemberByName($partial_name) {
+        $partial_name = preg_replace("/^[A-Za-z0-9]/", "", $partial_name);
+        foreach ($this->getGroupMembers() as $member) {
+            if (strpos($member->nickname, $partial_name) !== false) {
+                $this->logger->debug(sprintf("Found user '%s' searching for '%s'", $member->nickname, $partial_name));
+                return $member;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Returns an "attachment" for mentions in a message
+     */
+    protected function getMentions($message) {
+        $needle = "@";
+
+        if (strpos($message, $needle) !== false) {
+            $mentions = new \stdClass();
+            $mentions->type = "mentions";
+            $mentions->loci = array();
+            $mentions->user_ids = array();
+
+            $loci = array();
+            $lastPos = 0;
+            $positions = array();
+
+            // Search for all @'s in the message
+            while (($lastPos = strpos($message, $needle, $lastPos)) !== false) {
+                $positions[] = $lastPos;
+                $lastPos = $lastPos + strlen($needle);
+            }
+
+            // After each @, get the length of the sequence of characters until something non-alphanumeric is
+            // encoumtered
+            foreach ($positions as $p) {
+                $seq = array_values(array_filter(preg_split("/[^a-z0-9]/i", substr($message, $p))))[0];
+                $i = strpos($message, " ", $p);
+                $i = ($i === false) ? strlen($message) : $i;
+                $loci[] = array($p, $p + strlen($seq));
+            }
+
+            // Extract the string between each loci, and use it to search for users
+            foreach ($loci as $l) {
+                $str = substr($message, $l[0] + strlen($needle), $l[1] - $l[0]);
+                $id = $this->searchMemberByName($str);
+                if ($id !== false) {
+                    $mentions->loci[] = array($l[0], strlen($str) + 1);
+                    $mentions->user_ids[] = $id->user_id;
+                }
+            }
+
+            if (sizeof($mentions->user_ids) > 0) {
+                return $mentions;
+            }
+        }
     }
 }
 ?>
