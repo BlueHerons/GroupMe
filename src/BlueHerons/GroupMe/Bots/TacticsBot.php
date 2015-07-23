f201bb9cb1c071790654e0ac8b9f5a73b0ee751e
johnluetke <john@johnluetke.net>
2015-07-23 01:18:52 +0000

    Implement abstract methods

--- src/BlueHerons/GroupMe/Bots/TacticsBot.php
+++ src/BlueHerons/GroupMe/Bots/TacticsBot.php
@@ -3,12 +3,22 @@ namespace BlueHerons\GroupMe\Bots;
 
 class TacticsBot extends BaseBot {
 
+    protected $group_id;
+
     public function __construct($token) {
-        parent::_construct($token, "no_bot_id");
+        parent::__construct($token, "no_bot_id");
     }
 
     public function broadcast($group_id, $message) {
+        $this->group_id = $group_id;
         $this->sendMessage($message);
+    }
+
+    public function listen() {
+    }
+
+    protected function getGroupID() {
+        return $this->group_id;
     }
 }
 
