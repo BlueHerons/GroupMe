626742a68bdfe1704b0393b400c26c737dbc898e
johnluetke <john@johnluetke.net>
2015-10-23 20:45:07 +0000

    Refactor broadcast

--- src/BlueHerons/GroupMe/Bots/PMBot.php
+++ src/BlueHerons/GroupMe/Bots/PMBot.php
@@ -54,13 +54,13 @@ class PMBot extends HeronsBot {
     }
 
     public function broadcast() {
-        if ($this->isAdmin($this->getPayload()['sender_id'])) {
+        if ($this->isAdmin($this->payload->other_user->id)) {
             $message= implode(" ", array_slice(func_get_args(), 1));
-            $message = sprintf("[NETWORK BROADCAST FROM %s]\n\n%s", strtoupper($this->getPayload()['name']), $message);
+            $message = sprintf("** Broadcast from %s **\n\n%s", $this->payload->other_user->name, $message);
             $this->sendBroadcast($message);
         }
         else {
-            $this->replyToSender("Sorry, but only admins can use the \"broadcast\" command.");
+            $this->replyToSender("Sorry, you cannot use this command.");
         }
     }
 
